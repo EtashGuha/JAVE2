3fd1d6ea732cda7ae285e982b6f675468ae29118
Etash Guha <etashthebomb@gmail.com>
2020-10-16 22:57:42 -0400

    dB done

---
+++ ArticleList.py
@@ -0,0 +1,29 @@
+class ArticleList():
+	def __init__(self, model, topicfreq):
+		self.articles = {}
+		self.model = model
+		self.topicfreq = topicfreq
+
+	def add(article):
+		modelScore = model(article)
+		freqScore = topicfreq(article)
+		upvotes = 0
+		downvotes = 0
+		socScore = socialScore(upvotes, downvotes)
+		totalScore = (modelScore + freqScore + socScore)/3
+		self.articles[article] = (totalScore, modelScore, freqScore, socScore, upvotes, downvotes)
+
+	def socialScore(self, num_up, num_down):
+		return (num_up + 1)/(num_down + 1)
+
+	def upvote(article):
+		totalScore, modelScore, freqScore, socScore, upvotes, downvotes = self.articles[article]
+		upvotes += 1
+		socScore = socialScore(upvotes, downvotes) 
+		self.articles[article] = totalScore, modelScore, freqScore, socScore, upvotes, downvotes
+
+	def downvote(article):
+		totalScore, modelScore, freqScore, socScore, upvotes, downvotes = self.articles[article]
+		downvotes += 1
+		socScore = socialScore(upvotes, downvotes) 
+		self.articles[article] = totalScore, modelScore, freqScore, socScore, upvotes, downvotes
